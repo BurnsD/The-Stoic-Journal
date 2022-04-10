5fab8cbae9b26b83c2e5720ca8cd06e22964ffbc
BurnsD <Dustinburnsactech@gmail.com>
2022-04-10 14:38:48 -0500

    added public react files. set up react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import Login from '../src/components/Login';
+import NavBar from '../src/components/NavBar';
+import Journal from '../src/components/Journal';
+import Home from '../src/components/Home';
+import Qoutes from '../src/components/Qoutes';
+import Dashboard from './components/Dashboard';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    
-    </div>
+    <>
+    <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/journal' element={<Journal/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/qoutes' element={<Qoutes/>}/>
+      </Routes>
+    </>
   );
 }
 
